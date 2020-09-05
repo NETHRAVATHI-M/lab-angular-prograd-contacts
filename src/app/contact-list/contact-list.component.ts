07f4bc572387e6f9138af477f3610f1842c2ce3b
NETHRAVATHI-M <nethravathim72@gmail.com>
2020-09-05 22:17:10 +0530

    done

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -12,13 +12,23 @@ export class ContactListComponent implements OnInit {
   contacts: userContact[];
   newContact: any = {};
   constructor() { }
-
+newname='';
+email:'';
+phonenumber:'';
+image: any;
   ngOnInit() {
   // initialize your contacts here
   }
 
   addContact(newContact): any {
     console.log('Add contact has been called');
+    newContact={name:this.newname,
+    email:this.email,
+    phoneNumber:this.phonenumber,
+image:this.image}
+
+this.contacts.unshift(newContact);
+
      // Fill your code here
     // add contact to contacts list
     // clear inputs
